8fec6951872e238ecd93632b0d2309c2a2b0274a
CoderCheng <chengdavid2@gmail.com>
2019-07-17 16:22:38 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,36 +18,34 @@ def printstage(value):
 
 
 def main():
-    LOCAL_DIR = './'
-    DB_DIR = LOCAL_DIR + 'Database/FoodDB.csv'
-    Population_Number = 10
-    RECOMMENDED_DATA = [130,56,60] # [Carb, Protein, Fat] 19-30 year old man based off 1600 cal diet 
-	
-    #Import Database
-    printstage('Importing Database')
-    Food_DF = DatabaseHelper.import_DB(DB_DIR) #(Sid Implement Cleanup)
-	
-    #Initialize Population
-    printstage('Intializing Population')
-    Population_DF = GAOperators.initPop(Food_DF,Population_Number)
-	
-    printstage('Completing GA')
+	LOCAL_DIR = './'
+	DB_DIR = LOCAL_DIR + 'Database/FoodDB.csv'
+	Population_Number = 10
+	RECOMMENDED_DATA = [130,56,60] # [Carb, Protein, Fat] 19-30 year old man based off 1600 cal diet 
+
+	#Import Database
+	printstage('Importing Database')
+	Food_DF = DatabaseHelper.import_DB(DB_DIR) #(Sid Implement Cleanup)
+
+	#Initialize Population
+	printstage('Intializing Population')
+	Population_DF = GAOperators.initPop(Food_DF,Population_Number)
+
+	printstage('Completing GA')
     for i in range(2):
+		Population_DF = GAOperators.mutate(population_Df)
 
-        #Mutate (Sid)
-        Population_DF = GAOperators.mutate(population_Df)
-
-        #Evaluate Fitness of each chromosome
+		#Evaluate Fitness of each chromosome
 		fit_vals = fitness.fitness.value(population_df, Food_DF, RECOMMENDED_DATA)
 
-        #Select Parents (Jake)
-        Population_DF = GAOperators.Select_Parents(Population_DF,Population_Number)
+		#Select Parents (Jake)
+		Population_DF = GAOperators.Select_Parents(Population_DF, Population_Number)
 
-        print(Population_DF)
+		print(Population_DF)
 
-        #Create new offspring, Crossover (Dave)
+		#Create new offspring, Crossover 
 		child = crossover.crossover_child(parents, options, nvars, fitness, score, Population_DF)
 
 
 if __name__ == '__main__':
-    main()
+	main()
